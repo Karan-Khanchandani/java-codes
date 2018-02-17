e4ad9f4bd407479f3f57d6e6498dff2f9dc9a144
Karan Khanchandani <karan.khanchandani94@gmail.com>
2018-02-17 11:12:30 +0530

    6.1 Decreased time complexity

--- Problem_6_1_parity.java
+++ Problem_6_1_parity.java
@@ -10,17 +10,32 @@ import java.util.Scanner;
 
 public class Problem_6_1_parity{
 
+    public static short cp(long x){
+        x ^= (x >> 16);
+        x ^= (x >> 8);
+        x ^= (x >> 4);
+        x ^= (x >> 2);
+        x ^= (x >> 1);
+
+        return (short)(x & 0x1);
+    }
+
     public static short countParity(long x){
-        short result = 0;
-        while(x!=0){
-            result ^= (x&1);
-            x = x&(x-1);
+        final int WORD_SIZE = 16;
+        final int BIT_MASK = 0xFFFF;
+
+        int preComputeParity[] = new int[1<<16];
+        for( int i = 0; i < 1<<16; i++){
+            preComputeParity[i] = cp(i);
         }
-        return result;
+
+
+        return (short) (preComputeParity[(int) ((x >>> (3*WORD_SIZE)) & BIT_MASK)] ^ preComputeParity[(int) ((x >>> (2*WORD_SIZE)) & BIT_MASK)]
+        ^ preComputeParity[(int) ((x >>> (1*WORD_SIZE)) & BIT_MASK)] ^ preComputeParity[(int) (x & BIT_MASK)]);
     }
 
     public static void main(String[] args) {
-        System.out.println(countParity(55));
+        System.out.println(countParity(3));
     }
     
 }

c3aa79a67661a8ddb30759d41066f3887df5826c
Karan Khanchandani <karan.khanchandani94@gmail.com>
2018-02-28 21:46:16 +0530

    Problem 12.1 fix

--- Problem_12_1.java
+++ Problem_12_1.java
@@ -6,23 +6,24 @@ find first occurence of k in sorted array
 public class Problem_12_1{
 
     public static int binary_search(int arr[], int low, int high, int key){
-        if(low > high)
-            return -1;
-        
-        int mid = low + (high - low)/2;
-
-        if(arr[mid] == key && low == mid){
-            return low;
+        int result  = -1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(arr[mid] == key){
+                result = mid;
+                high = mid - 1;
+            }
+            else if(arr[mid] > key){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
         }
-
-        if(arr[mid] >= key)
-            return binary_search(arr, low, mid -1, key);
-        else
-            return binary_search(arr, mid + 1, high, key);
+        return result;
     }
 
     public static void main(String[] args) {
-        int arr[] = new int[]{-14, -10, 2 ,108 ,108 ,243 ,285 ,285 ,285 ,401};
+        int arr[] = new int[]{-14, -10, 2 ,108 ,108 ,243 ,285 ,285 ,285 ,401, 500};
 
         int key = 108;
         int index = binary_search(arr, 0, arr.length-1 , key);
@@ -45,6 +46,10 @@ public class Problem_12_1{
         System.out.println(key + " -> " + index);
 
         key = -100;
+        index = binary_search(arr, 0, arr.length-1 , key);
+        System.out.println(key + " -> " + index);
+
+        key = 243;
         index = binary_search(arr, 0, arr.length-1 , key);
         System.out.println(key + " -> " + index);
     }

c0fedbd83e501a58db71de6953f59027996d6a9b
Karan Khanchandani <karan.khanchandani94@gmail.com>
2018-03-07 15:31:43 +0530

    Problem 18.2

---
+++ Problem_18_2.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/*
+Problem Description:
+Schedule to minimize waiting time
+*/
+
+public class Problem_18_2{
+    
+    public static int minimize_time(List<Integer> service_time){
+        Collections.sort(service_time);
+        int result = 0;
+        for(int i = 0; i < service_time.size(); i++){
+            int num_remaining_queries = service_time.size() - (i+1);
+            result += service_time.get(i)*num_remaining_queries;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        List<Integer> service_time = new ArrayList<>(Arrays.asList(2, 5, 1, 3));
+        System.out.println(minimize_time(service_time));
+    }
+}

0aa35aa01a1a7eee966c61229dd317aaeb37c742
Karan Khanchandani <karan.khanchandani94@gmail.com>
2018-05-13 11:41:15 +0530

    Create LeetCode_832.java

---
+++ LeetCode_832.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] A) {
+        if(A == null || A[0].length == 0){
+            return A;
+        }
+        int n = A.length;
+        for(int i = 0 ; i < A.length; i++){
+            for(int j = 0; j < A.length/2; j++){
+                int temp = A[i][j];
+                A[i][j] = A[i][n-1-j];
+                A[i][n-1-j] = temp;
+            }
+        }
+        
+        for(int i = 0 ; i < A.length; i++){
+            for(int j = 0; j < A.length; j++){
+                if(A[i][j] == 0){
+                    A[i][j] =1;
+                }else{
+                    A[i][j] = 0;
+                }
+            }
+        }
+        
+        return A;
+    }
+}

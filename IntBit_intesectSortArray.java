168e7e5db7aabfa9bb841e51d4603259512b1e95
Karan Khanchandani <karan.khanchandani94@gmail.com>
2018-05-21 14:33:21 +0530

    Create IntBit_intesectSortArray.java

---
+++ IntBit_intesectSortArray.java
@@ -0,0 +1,18 @@
+public class Solution {
+    // DO NOT MODIFY THE LIST. IT IS READ ONLY
+    public ArrayList<Integer> intersect(final List<Integer> A, final List<Integer> B) {
+        ArrayList<Integer> res = new ArrayList<>();
+        int i = 0, j =0;
+        while(i < A.size() && j < B.size()){
+            if(A.get(i).intValue() == B.get(j).intValue()){
+                res.add(A.get(i));
+                i++;j++;
+            }else if(A.get(i).intValue() < B.get(j).intValue()){
+                i++;
+            }else{
+                j++;
+            }
+        }
+        return res;
+    }
+}

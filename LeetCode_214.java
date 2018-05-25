ae88c2c1003a199d840a99d933b19b145637a8fb
Karan Khanchandani <karan.khanchandani94@gmail.com>
2018-05-25 12:59:45 +0530

    Create LeetCode_214.java

---
+++ LeetCode_214.java
@@ -0,0 +1,46 @@
+class Solution {
+    static char[] T, P;
+    static int[] b;
+    static int n,m;
+    
+    static void kmpProcess(){
+        b = new int[m+1];
+        b[0] = -1;
+        int i = 0, j = -1;
+        while(i < m){
+            while(j >= 0  && P[i] != P[j])j = b[j];
+            i++;j++;
+            b[i] = j;
+        }
+        
+    }
+    
+    static int kmpMatching(){
+        kmpProcess();
+        int i = 0, j = 0;
+        while(i < n){
+            while(j >= 0 && T[i] != P[j])j = b[j];
+            i++;j++;
+            
+            if(j == m){
+                j = -1;
+            }
+            
+        }
+        return j;
+    }
+    
+    public String shortestPalindrome(String s) {
+        String t = new StringBuilder(s).reverse().toString();
+        T = t.toCharArray();
+        String p= s;
+        P = p.toCharArray();
+        n = m =  T.length;
+        int tail = kmpMatching();
+        if(tail == -1){
+            return s;
+        }else{
+            return t + p.substring(tail);
+        }
+    }
+} 

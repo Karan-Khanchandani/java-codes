c55d06b984ca75c9a430e46b9d1b6de0dc9bff1c
Karan Khanchandani <karan.khanchandani94@gmail.com>
2018-06-09 11:28:43 +0530

    Create IntBit_TripletBet1and2.java

---
+++ IntBit_TripletBet1and2.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public int solve(ArrayList<String> A) {
+        ArrayList<Double> res = new ArrayList<>();
+        for(int i = 0; i < A.size(); i++){
+            res.add(Double.parseDouble(A.get(i)));
+        }
+        if(res.size() < 3) return 0;
+        double a = res.get(0), b = res.get(1), c = res.get(2);
+        for(int i = 3; i < res.size(); i++){
+            double sum = a+b+c;
+            if(Double.compare(sum, 1) > 0 && Double.compare(sum, 2) < 0)return 1;
+            if(Double.compare(sum, 1) < 0){
+                if(Double.compare(a, b) < 0 && Double.compare(a, c) < 0) a = res.get(i);
+                else if(Double.compare(b, a) < 0 && Double.compare(b, c) < 0) b= res.get(i);
+                else c = res.get(i);
+            }
+            if(Double.compare(sum, 2) > 0){
+                if(Double.compare(a, b) > 0 && Double.compare(a, c) > 0) a = res.get(i);
+                else if(Double.compare(b, a) > 0 && Double.compare(b, c) > 0) b= res.get(i);
+                else c = res.get(i);
+            }
+        }
+        
+        double sum  = a + b+ c;
+        if(Double.compare(sum, 1) > 0 && Double.compare(sum, 2) < 0)return 1;
+        
+        return 0;
+    }
+}

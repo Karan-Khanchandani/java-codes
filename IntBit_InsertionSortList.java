eee7ebd5bc178d9774a7a41d6272bdd4e42dca48
Karan Khanchandani <karan.khanchandani94@gmail.com>
2018-06-15 10:42:12 +0530

    Create IntBit_InsertionSortList.java

---
+++ IntBit_InsertionSortList.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+public class Solution {
+    public ListNode insertionSortList(ListNode A) {
+       if(A == null || A.next == null) return A;
+       
+       ListNode ret = null;
+       while(A != null){
+           ListNode curr = A;
+           A = A.next;
+           if(ret == null || curr.val < ret.val){
+               curr.next = ret;
+               ret = curr;
+           }else{
+               ListNode temp = ret;
+               while(temp != null){
+                   if(temp.next == null || curr.val < temp.next.val){
+                       curr.next = temp.next;
+                   temp.next = curr;
+                    break; // done
+                   }
+                   temp = temp.next;
+               }
+           }
+       }
+        return ret;
+    }
+}

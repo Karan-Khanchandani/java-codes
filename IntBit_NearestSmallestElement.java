2084a08e3549341bf5fdd1d85478cadda5212b7f
Karan Khanchandani <karan.khanchandani94@gmail.com>
2018-06-25 10:28:52 +0530

    Create IntBit_NearestSmallestElement.java

---
+++ IntBit_NearestSmallestElement.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public ArrayList<Integer> prevSmaller(ArrayList<Integer> A) {
+        if(A == null || A.size() == 0)return A;
+        
+        Stack<Integer> st = new Stack<>();
+        ArrayList<Integer> res = new ArrayList<>();
+        for(int i = 0; i < A.size(); i++){
+            int curr = A.get(i);
+            while(!st.isEmpty() && st.peek() >= curr)st.pop();
+            
+            if(st.isEmpty())res.add(-1);
+            else res.add(st.peek());
+            st.push(curr);
+        }
+        return res;
+    }
+}

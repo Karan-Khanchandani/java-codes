c8f6dd39946663c63fdac7b7e82de3e5eaf8d204
Karan Khanchandani <karan.khanchandani94@gmail.com>
2018-07-02 10:14:07 +0530

    Create IntBit_isSameTree.java

---
+++ IntBit_isSameTree.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) {
+ *      val = x;
+ *      left=null;
+ *      right=null;
+ *     }
+ * }
+ */
+public class Solution {
+    public int isSameTree(TreeNode A, TreeNode B) {
+        if(A == null && B == null) return 1;
+        if(A == null && B != null) return 0;
+        if(A != null && B == null) return 0;
+        
+        int left = isSameTree(A.left, B.left);
+        int right = isSameTree(A.right, B.right);
+        
+        if(left == 1 && right == 1 && A.val == B.val) return 1;
+        return 0;
+    }
+}

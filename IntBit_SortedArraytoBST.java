3d3c3bc4662d104b349f71c764221a886d9eea35
Karan Khanchandani <karan.khanchandani94@gmail.com>
2018-07-04 00:23:41 +0530

    Create IntBit_SortedArraytoBST.java

---
+++ IntBit_SortedArraytoBST.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode sortedArrayToBST(final List<Integer> a) {
+        return sortedArrayToBSTUtil(a, 0, a.size() - 1);
+    }
+    
+    public TreeNode sortedArrayToBSTUtil(final List<Integer> a, int start, int end){
+        if(start > end) return null;
+        int mid = start + (end - start)/2;
+        TreeNode root = new TreeNode(a.get(mid));
+        root.left = sortedArrayToBSTUtil(a,start, mid - 1);
+        root.right = sortedArrayToBSTUtil(a,mid+1,end);
+        return root;
+    }
+}

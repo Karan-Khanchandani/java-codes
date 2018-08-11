dd65b361cd6923af6a5bac728d16418c81fb07cf
Karan Khanchandani <karan.khanchandani94@gmail.com>
2018-08-11 20:17:37 +0530

    Create IntBit_InorderCartesianTree.java

---
+++ IntBit_InorderCartesianTree.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) {
+ *      val = x;
+ *      left=null;
+ *      right=null;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode buildTree(ArrayList<Integer> A) {
+        TreeNode root = null;
+        if(A.size() == 0) return root;
+        root = buildTreeUtil(A, 0, A.size() - 1);
+        return root;
+    }
+    
+    public TreeNode buildTreeUtil(ArrayList<Integer> A, int start, int end){
+        if(start > end) return null;
+        int max = findMax(A,start,end);
+        TreeNode root = new TreeNode(A.get(max));
+        root.left = buildTreeUtil(A, start, max - 1);
+        root.right = buildTreeUtil(A, max + 1, end);
+        return root;
+    }
+    
+    public int findMax(ArrayList<Integer> A, int start, int end){
+        int maxVal = start;
+        for(int i = start; i <= end; i++){
+            if(A.get(maxVal) < A.get(i)){
+                maxVal = i;
+            }
+        }
+        return maxVal;
+    }
+}

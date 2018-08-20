4e859eb3f5a5aaa261e53860fcaa447d5bead543
Karan Khanchandani <karan.khanchandani94@gmail.com>
2018-08-20 12:49:09 +0530

    Create IntBit_DistributeCandies.java

---
+++ IntBit_DistributeCandies.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int candy(ArrayList<Integer> A) {
+        int size=A.size();
+         if(size<=1)
+             return size;
+         int num[] = new int[size];
+         Arrays.fill(num, 1);
+         for (int i = 1; i < size; i++)
+         {
+             if(A.get(i)>A.get(i-1))
+                 num[i]=num[i-1]+1;
+         }
+         for (int i= size-1; i>0 ; i--)
+         {
+             if(A.get(i-1)>A.get(i))
+                 num[i-1]=Math.max(num[i]+1,num[i-1]);
+         }
+         int result=0;
+         for (int i = 0; i < size; i++)
+         {
+             result+=num[i];
+         }
+         return result;
+    }
+}

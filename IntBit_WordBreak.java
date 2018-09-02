c4f4d2c3c58787cf67ef476f3de9f231756cd4b7
Karan Khanchandani <karan.khanchandani94@gmail.com>
2018-09-02 10:51:22 +0530

    Create IntBit_WordBreak.java

---
+++ IntBit_WordBreak.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int wordBreak(String A, ArrayList<String> B) {
+        boolean[] dp = new boolean[A.length() + 1];
+        Set<String> set = new HashSet<>();
+        set.addAll(B);
+        dp[0] = true;
+        for(int i = 1; i <= A.length(); i++){
+            for(int j = i-1; j >=0 ; j--){
+                dp[i] = dp[j] && set.contains(A.substring(j, i));
+                if(dp[i]) break;
+            }
+        }
+        
+        return dp[A.length()] ? 1 : 0;
+    }
+}

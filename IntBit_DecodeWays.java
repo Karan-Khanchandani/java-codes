546d57c95c3126fa422e9fbc8f0131d92264adb8
Karan Khanchandani <karan.khanchandani94@gmail.com>
2018-09-11 21:11:13 +0530

    Create IntBit_DecodeWays.java

---
+++ IntBit_DecodeWays.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int numDecodings(String A) {
+        int n = A.length();
+        if (n == 0) return 0;
+        
+        int[] memo = new int[n+1];
+        memo[n]  = 1;
+        memo[n-1] = A.charAt(n-1) != '0' ? 1 : 0;
+        
+        for (int i = n - 2; i >= 0; i--)
+            if (A.charAt(i) == '0') continue;
+            else memo[i] = (Integer.parseInt(A.substring(i,i+2))<=26) ? memo[i+1]+memo[i+2] : memo[i+1];
+        
+        return memo[0];
+    }
+}
